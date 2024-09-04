fa66adc2953d75bbf92ed9cc8153121f2c551392
kensstark <tadiwakennedy@gmail.com>
2024-09-04 16:44:07 +0800

    Added AddTask Page on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,10 @@ export default function Home() {
         <BacklogCard /> {/* Add BacklogCard component here */}
       </div>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <button onClick={openModal} className="bg-blue-500 text-white px-4 py-2 rounded">
+        <button
+          onClick={openModal}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
           Open Pop Up
         </button>
       </div>
@@ -35,7 +38,7 @@ export default function Home() {
         <h2 className="text-xl font-bold mb-4">Task Form</h2>
         {/*<DropDown /> {/* Adding your DropDown component */}
       </section>
-      {/*<AddTaskPage />*/}
+      <AddTaskPage />
     </main>
   );
 }
