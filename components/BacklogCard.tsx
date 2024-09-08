a5e925d6e5433f6a10c26b7832b03faa9a7c9084
jumpsuitsushi <jcho0186@student.monash.edu>
2024-09-08 16:52:12 +0800

    change individual task info and delete delete task button colour

--- components/BacklogCard.tsx
+++ components/BacklogCard.tsx
@@ -85,7 +85,7 @@ const BacklogCard = () => {
           </div>
           <div className="space-x-4">
             <button 
-              className="px-4 py-2 bg-gray-300 text-gray-700 font-semibold rounded-md shadow-md hover:bg-gray-400"
+              className={`px-4 py-2 ${isInvisible?`bg-red-500 text-white`:`bg-gray-300 text-gray-700`} font-semibold rounded-md shadow-md active:ring active:ring-red-600 hover:bg-gray-400`}
               onClick={() => SetIsInvisible(!isInvisible)}
             >
               Delete Task
--- components/IndividualTask.tsx
+++ components/IndividualTask.tsx
@@ -3,59 +3,59 @@
  * such as task title, description, assigned personnel, progress status, edit history, and tags.
  */
 
+import { TaskData } from "@/utils/interface";
 import React from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { IoMdArrowDropup } from "react-icons/io";
-import {TaskData} from "@/utils/interface";
 
 
 interface IndividualTaskInfoProps {
   taskData: TaskData;
 }
 
-const IndividualTaskInfo = ({taskData}: IndividualTaskInfoProps)=> {
+const colour = ['bg-purple-400','bg-blue-300','bg-red-500','bg-yellow-400 ']
+
+const IndividualTaskInfo= ({taskData}:IndividualTaskInfoProps)=> {
   return (
     <div className="w-full">
       <div className="p-4">
-        <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center justify-between">
           <div className="flex items-center">
             <div className="bg-purple-500 text-white rounded-full h-10 w-10 flex items-center justify-center">
-              JQ
+              {(taskData.assignedTo)? (taskData.assignedTo).charAt(0).toUpperCase(): ''}
             </div>
-            <h2 className="text-2xl font-semibold ml-4 ">{taskData.taskName? taskData.taskName : "Title Not Found"}</h2>
+            <h2 className="text-2xl font-semibold ml-4 ">{taskData.taskName?taskData.taskName:'Unknown task name'}</h2>
           </div>
           <button className="text-gray-400 hover:text-gray-600 mb-2 mr-4">
             <FaRegEdit size={20}/>
           </button>
         </div>
 
-        <p className="text-gray-700">Description: this is the description of important task 1</p>
+        <p className="mt-4 text-gray-700">Description: {taskData.description?taskData.description:''}</p>
 
         <div className="mt-4 flex flex-wrap space-x-2">
-          <span className="inline-block bg-purple-400 text-white rounded-full px-3 py-1 text-sm font-semibold">#tag1</span>
-          <span className="inline-block bg-blue-300 text-white rounded-full px-3 py-1 text-sm font-semibold">#tag2</span>
-          <span className="inline-block bg-red-500 text-white rounded-full px-3 py-1 text-sm font-semibold">#tag3</span>
-          <span className="inline-block bg-yellow-400 text-white rounded-full px-3 py-1 text-sm font-semibold">#tag4</span>
+          {Array.from({length:((taskData.tags)? taskData.tags.length : 0)}, (_,i) => (
+            <span className={`inline-block ${colour[i % 4]} text-white rounded-full px-3 py-1 text-sm font-semibold`}>{taskData.tags? taskData.tags[i]:''}</span>
+          ))}
         </div>
-        
 
         <div className="mt-6 space-y-4">
-            <p><strong>Assigned to:</strong> Jia Qian</p>
-            <p><strong>Finished by:</strong> 11/09/2024 </p>
+            <p><strong>Assigned to: </strong>{taskData.assignedTo}</p>
+            <p><strong>Finished by: </strong>{taskData.finishedBy}</p>
           </div>
 
           <div className="mt-4 grid grid-cols-2 gap-3">
             <div>
-              <p><strong>Progress status:</strong> <span className="inline-block bg-blue-300 text-black rounded-full px-3 py-1 text-sm font-semibold">not started</span></p>
+              <p><strong>Progress status: </strong> <span className="inline-block bg-blue-300 text-black rounded-full px-3 py-1 text-sm font-semibold">{taskData.status?taskData.status:'not started'}</span></p>
             </div>
             <div>
-              <p><strong>Storypoint:</strong> <span className="inline-block bg-red-300 text-black rounded-full px-3 py-1 text-sm font-semibold">3 / 10</span></p>
+              <p><strong>Storypoint: </strong> <span className="inline-block bg-red-300 text-black rounded-full px-3 py-1 text-sm font-semibold">{taskData.storyPoint?taskData.storyPoint:0} / 10</span></p>
             </div>
             <div>
-              <p><strong>Type:</strong> user story</p>
+              <p><strong>Type: </strong>{taskData.type?taskData.type:'story'}</p>
             </div>
             <div>
-              <p><strong>Priority:</strong> medium</p>
+              <p><strong>Priority: </strong>{taskData.priority?taskData.priority:'not important'}</p>
             </div>
           </div>
           
@@ -68,7 +68,7 @@ const IndividualTaskInfo = ({taskData}: IndividualTaskInfoProps)=> {
             </button>
           </div>
             
-          <ul className="list-none">
+          <ul className="list-none mb-6">
             <li className="flex justify-start items-center bg-purple-100 p-2 rounded mb-1 gap-3">
               <div className="ml-1.5 bg-purple-500 text-white rounded-full h-8 w-8 flex items-center justify-center">
                 V
@@ -79,7 +79,7 @@ const IndividualTaskInfo = ({taskData}: IndividualTaskInfoProps)=> {
               <div className="ml-1.5 bg-purple-500 text-white rounded-full h-8 w-8 flex items-center justify-center">
                 S
               </div>
-              <span className="text-sm">Edited by Shanwu at 2.20pm</span>
+              <span className="text-sm">Edited by Shanwu at 2.20pm</span>  
             </li>
             <li className="flex justify-start items-center bg-purple-100 p-2 rounded mb-1 gap-3">
               <div className="ml-1.5 bg-purple-500 text-white rounded-full h-8 w-8 flex items-center justify-center">
