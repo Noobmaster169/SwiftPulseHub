e2d8c586924961bd824eaf15fc310f5004bd77aa
jumpsuitsushi <jcho0186@student.monash.edu>
2024-10-06 17:14:13 +0800

    feat: Implemented admin and user login pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,10 +4,15 @@ import ThemeSelector from "@/components/ThemeSelector";
 import PopUp from "@/components/PopUp";
 import { useTheme } from "@/components/ThemeContext";
 import {useState} from 'react';
+import LoginPage from "@/components/LoginPage";
+import AdminTeamBoard from "@/components/AdminTeamBoard";
+import UserTeamBoard from "@/components/UserTeamBoard";
 
 export default function Dashborad(){
     const { currentTheme, setCurrentTheme } = useTheme();
     const [isThemeSelectorOpen, setIsThemeSelectorOpen] = useState<boolean>(false);
+    const [isAdminLogin, setIsAdminLogin] = useState<boolean>(false);
+    const [isUserLogin, setIsUserLogin] = useState<boolean>(false);
 
     const handleThemeChange = (newTheme: string) => {
         setCurrentTheme(newTheme);
@@ -22,8 +27,14 @@ export default function Dashborad(){
             backgroundAttachment: 'fixed',
             backgroundSize: 'cover',
             backgroundPosition: 'center' }}
-        >
-            <div>Empty</div>
+        >   
+            
+            <div className="flex-1 flex flex-col items-center justify-between p-4 ml-64">
+                <div className="w-full mt-12">
+                    {isAdminLogin? <AdminTeamBoard/>: isUserLogin ? <UserTeamBoard/> : <LoginPage setAdminLogin={setIsAdminLogin} setUserLogin={setIsUserLogin}/> }
+                </div>
+            </div>
+                
 
             <PopUp isOpen={isThemeSelectorOpen} setIsOpen={setIsThemeSelectorOpen}>
                 <ThemeSelector 
