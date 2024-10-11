61c5b4239c1ce047738285d8e501c52e0a9d4b1e
jumpsuitsushi <jcho0186@student.monash.edu>
2024-10-11 18:08:23 +0800

    feat: Implemented sign-in and sign-up form

---
+++ components/signUpForm.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import {CardTitle, CardHeader, CardContent, CardFooter, Card} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+export function SignupForm() {
+  return (
+    <div className="w-full max-w-md">
+      <form>
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-3xl text-center font-bold">Sign Up</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" name="username" type="text" placeholder="username"/>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" name="email" type="email" placeholder="name@example.com"/>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" name="password" type="password" placeholder="password"/>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <button className="w-32 mt-4 rounded bg-black text-sm text-white">Create account</button>
+          </CardFooter>
+        </Card>
+        <div className="mt-4 text-center text-sm">
+          Have an account?
+          <Link className="underline ml-2" href="signIn">
+            Sign In
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
